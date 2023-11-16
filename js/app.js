3c6d6a1756836ab2695deb1b65c9f36f60b32882
kunjan71 <kunjankishor71@gmail.com>
2023-11-16 15:52:38 +0530

    real video added

--- js/app.js
+++ js/app.js
@@ -8253,6 +8253,165 @@ PERFORMANCE OF THIS SOFTWARE.
 			});
 		}
 
+// ------------------------intro video------------------------------------
+
+// Select the elements 
+const video = document.getElementById("video"); 
+const videoThumbnail = document.getElementById("video-thumbnail"); 
+const playpause = document.getElementById("play-pause"); 
+const frwd = document.getElementById("skip-10"); 
+const bkwrd = document.getElementById("skipminus-10"); 
+const volume = document.getElementById("volume"); 
+const mutebtn = document.getElementById("mute"); 
+const videoContainer = document.querySelector(".video-container"); 
+const controls = document.querySelector(".controls"); 
+const progressBar = document.querySelector(".progress-bar"); 
+const playbackline = document.querySelector(".playback-line"); 
+const currentTimeRef = document.getElementById("current-time"); 
+const maxDuration = document.getElementById("max-duration"); 
+
+const timeFormatter = (timeInput) => { 
+	let minute = Math.floor(timeInput / 60); 
+	minute = minute < 10 ? "0" + minute : minute; 
+	let second = Math.floor(timeInput % 60); 
+	second = second < 10 ? "0" + second : second; 
+	return `${minute}:${second}`; 
+}; 
+
+// Play-Pause 
+playpause.addEventListener("click", function () { 
+	if (video.paused) { 
+		videoThumbnail.style.display = "none"; 
+		video.play(); 
+		playpause.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>'; 
+	} else { 
+		video.pause(); 
+		playpause.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>'; 
+	} 
+}); 
+
+let isPlaying = false; 
+
+// Function to toggle play/pause 
+function togglePlayPause() { 
+	if (isPlaying) { 
+		video.pause(); 
+		playpause.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>'; 
+	} else { 
+		videoThumbnail.style.display = "none"; 
+		video.play(); 
+		playpause.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>'; 
+	} 
+	isPlaying = !isPlaying; 
+} 
+
+document.addEventListener("keydown", function (event) { 
+	if (event.key === 32 || event.key === " ") { 
+		event.preventDefault(); 
+
+		// Prevent scrolling the page down 
+		togglePlayPause(); 
+	} 
+}); 
+
+// Event listener for the video to 
+// update the isPlaying flag 
+video.addEventListener("play", function () { 
+	isPlaying = true; 
+}); 
+
+video.addEventListener("pause", function () { 
+	isPlaying = false; 
+}); 
+
+video.addEventListener("ended", function () { 
+	playpause.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>'; 
+}); 
+
+// Forward 5 sec or backward 5 sec 
+frwd.addEventListener("click", function () { 
+	video.currentTime += 5; 
+}); 
+bkwrd.addEventListener("click", function () { 
+	video.currentTime -= 5; 
+}); 
+
+// Mute or Unmute 
+mutebtn.addEventListener("click", function () { 
+	if (video.muted) { 
+		video.muted = false; 
+		mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>'; 
+		volume.value = video.volume; 
+	} else { 
+		video.muted = true; 
+		mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>'; 
+		volume.value = 0; 
+	} 
+}); 
+
+document.addEventListener("keydown", function (event) { 
+	if (event.key === "M" || event.key === "m") { 
+		event.preventDefault(); 
+		if (video.muted) { 
+			video.muted = false; 
+			mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>'; 
+			volume.value = video.volume; 
+		} else { 
+			video.muted = true; 
+			mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>'; 
+			volume.value = 0; 
+		} 
+	} 
+}); 
+
+volume.addEventListener("input", function () { 
+	video.volume = volume.value; 
+	if (video.volume === 0) { 
+		mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>'; 
+	} else { 
+		mutebtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>'; 
+	} 
+}); 
+
+// Hide or unhide controllers div 
+videoContainer.addEventListener("mouseenter", () => { 
+	controls.style.opacity = 1; 
+}); 
+
+videoContainer.addEventListener("mouseleave", () => { 
+	controls.style.opacity = 0; 
+}); 
+
+// Update the playback line as the video plays 
+video.addEventListener("timeupdate", () => { 
+	const currentTime = video.currentTime; 
+	const duration = video.duration; 
+	const percentage = (currentTime / duration) * 100; 
+	progressBar.style.width = percentage + "%"; 
+}); 
+
+function showThumbnail() { 
+	videoThumbnail.style.display = "block"; 
+} 
+
+// Reseting the playback line when the video ends 
+video.addEventListener("ended", () => { 
+	progressBar.style.width = "0%"; 
+	showThumbnail(); 
+}); 
+
+setInterval(() => { 
+	currentTimeRef.innerHTML = timeFormatter(video.currentTime); 
+	maxDuration.innerText = timeFormatter(video.duration); 
+}, 1); 
+
+playbackline.addEventListener("click", (e) => { 
+	let timelineWidth = playbackline.clientWidth; 
+	video.currentTime = (e.offsetX / timelineWidth) * video.duration; 
+}); 
+
+
+
 //   //   Screen detail script---------------------------------------------
 
 // let width;
@@ -8283,3 +8442,5 @@ PERFORMANCE OF THIS SOFTWARE.
 //   }
   
 //       window.addEventListener("resize", onChangeWindow);
+
+
